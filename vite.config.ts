54d38ccdaad644a9f977d134e20990b79e1f1546
Bitchiko Tchelidze <Bitchiko.Tchelidze@mt.com>
2025-11-25 20:49:04 +0400

    fixed vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Must match the GitHub repo name for project pages
-  base: '/free-cv-builder/',
+  base: '/',
 });
 
